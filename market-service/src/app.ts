dd8bfbd443d163d2f4b248291219132ec1c7d0cd
Piyush <mr.piyush.cv@gmail.com>
2025-03-15 19:06:53 +0530

    market-service

---
+++ market-service/src/app.ts
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req : Request, res : Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
